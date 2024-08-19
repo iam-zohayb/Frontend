54e9bd638129f334c4b0a2141645bc43849c099b
iam-zohaib <114738880+iam-zohaib@users.noreply.github.com>
2024-08-19 20:26:18 +0500

    e

--- vite/src/components/Navbar.jsx
+++ vite/src/components/Navbar.jsx
@@ -8,10 +8,12 @@ const Navbar = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-16 items-center justify-between">
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="flex flex-shrink-0 items-center">
-                <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
-              </div>
-              <div className="hidden sm:ml-6 sm:block">
+            <div className="flex flex-shrink-0 flex-col items-center">
+  <img className="h-10 w-12" src="/logo.jpeg" alt="Your Company" />
+  <div className="" style={{fontSize:'9px',color:'white'}}>Ezeehollytrip</div>
+</div>
+
+              <div className="hidden sm:ml-6 sm:block mt-2">
                 <div className="flex space-x-4">
                   <NavLink 
                     to="/form"
